8f963e4bbc6e1157cf48d65fcd35efcfeb41925d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2014-12-02 23:40:16 +0100

    Add Yo greetings and list of examples

--- app/index.js
+++ app/index.js
@@ -1,30 +1,110 @@
 'use strict';
-var util = require('util');
+var chalk = require('chalk');
 var path = require('path');
+var util = require('util');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 
 var BootstrapExamplesGenerator = yeoman.generators.Base.extend({
-  initializing: function () {
+  initializing: function() {
     this.pkg = require('../package.json');
   },
 
-  prompting: function () {
+  prompting: function() {
     var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the praiseworthy BootstrapExamples generator!'
-    ));
-
+    if (!this.options['skip-welcome-message']) {
+      this.log(yosay(
+        'Welcome to the Bootstrap 3.* Examples generator!'
+      ));
+      this.log(chalk.magenta(
+        'Out of the box latest CSS distribution of Bootstrap 3.* is used with jQuery, Grunt and all tools.'
+      ));
+      this.log(chalk.magenta(
+        'Just pick an offical example from the list below to start your project'
+      ));
+    }
+    var inquirer = yeoman.inquirer;
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'list',
+      name: 'examples',
+      message: 'Which offical example use to build the project?',
+      paginated: true,
+      choices: [new inquirer.Separator('--- Using the framework ---'), {
+        name: 'Starter template',
+        value: 'starterTemplate',
+        checked: true
+      }, {
+        name: 'Theme',
+        value: 'theme',
+        checked: false
+      }, {
+        name: 'Grids',
+        value: 'grids',
+        checked: false
+      }, {
+        name: 'Jumbotron',
+        value: 'jumbotron',
+        checked: false
+      }, {
+        name: 'Jumbotron narrow',
+        value: 'jumbotron-narrow',
+        checked: false
+      }, new inquirer.Separator('--- Navbars in action ---'), {
+        name: 'Navbar',
+        value: 'navbar',
+        checked: false
+      }, {
+        name: 'Static top navbar',
+        value: 'navbar-static-top',
+        checked: false
+      }, {
+        name: 'Fixed navbar',
+        value: 'navbar-fixed-top',
+        checked: false
+      }, new inquirer.Separator('--- Custom components ---'), {
+        name: 'Cover',
+        value: 'cover',
+        checked: false
+      }, {
+        name: 'Carousel',
+        value: 'carousel',
+        checked: false
+      }, {
+        name: 'Blog',
+        value: 'blog',
+        checked: false
+      }, {
+        name: 'Dashboard',
+        value: 'dashboard',
+        checked: false
+      }, {
+        name: 'Sign-in page',
+        value: 'signin',
+        checked: false
+      }, {
+        name: 'Justified nav',
+        value: 'justified-nav',
+        checked: false
+      }, {
+        name: 'Sticky footer',
+        value: 'sticky-footer',
+        checked: false
+      }, {
+        name: 'Sticky footer with navbar',
+        value: 'sticky-footer-navbar',
+        checked: false
+      }, new inquirer.Separator('--- Experiments ---'), {
+        name: 'Non-responsive Bootstrap',
+        value: 'non-responsive',
+        checked: false
+      }, {
+        name: 'Offcanvas',
+        value: 'offcanvas',
+        checked: false
+      }]
     }];
 
-    this.prompt(prompts, function (props) {
+    this.prompt(prompts, function(props) {
       this.someOption = props.someOption;
 
       done();
@@ -32,7 +112,7 @@ var BootstrapExamplesGenerator = yeoman.generators.Base.extend({
   },
 
   writing: {
-    app: function () {
+    app: function() {
       this.dest.mkdir('app');
       this.dest.mkdir('app/templates');
 
@@ -40,13 +120,13 @@ var BootstrapExamplesGenerator = yeoman.generators.Base.extend({
       this.src.copy('_bower.json', 'bower.json');
     },
 
-    projectfiles: function () {
+    projectfiles: function() {
       this.src.copy('editorconfig', '.editorconfig');
       this.src.copy('jshintrc', '.jshintrc');
     }
   },
 
-  end: function () {
+  end: function() {
     this.installDependencies();
   }
 });
